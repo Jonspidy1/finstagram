8e295166641d3611af0306063df5f69e2f3c65c6
Jonspidy1 <cameronokutan@outlook.com>
2021-10-05 01:16:05 +0000

    W3D1

--- app/actions.rb
+++ app/actions.rb
@@ -7,11 +7,11 @@ def humanized_time_ago(time_ago_in_minutes)
 end
 
 get '/' do
-  finstagram_post_shark = {
+  @finstagram_post_shark = {
     username: "sharky_j",
     avatar_url: "http://naserca.com/images/sharky_j.jpg",
     photo_url: "http://naserca.com/images/shark.jpg",
-    time_ago_in_minutes: 15,
+    humanized_time_ago: humanized_time_ago(15),
     like_count: 0,
     comment_count: 1,
     comments: [{
@@ -20,7 +20,7 @@ get '/' do
     }]
   }
 
-  finstagram_post_whale = {
+  @finstagram_post_whale = {
     username: "kirk_whalum",
     avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
     photo_url: "http://naserca.com/images/whale.jpg",
@@ -33,7 +33,7 @@ get '/' do
     }]
   }
 
-  finstagram_post_marlin = {
+  @finstagram_post_marlin = {
     username: "marlin_peppa",
     avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
     photo_url: "http://naserca.com/images/marlin.jpg",
@@ -45,6 +45,8 @@ get '/' do
       text: "lunchtime! ;)"
     }]
   }
-  [finstagram_post_shark, finstagram_post_whale, finstagram_post_marlin].to_s
+  @finstagram_posts = [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin]
+  
+  erb(:index)
 end
   
