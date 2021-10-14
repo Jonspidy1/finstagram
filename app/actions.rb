99146cc65ae66650b989ce99923871303e452427
Jonspidy1 <cameronokutan@outlook.com>
2021-10-14 00:13:30 +0000

    W4D2

--- app/actions.rb
+++ app/actions.rb
@@ -1,13 +1,46 @@
-def humanized_time_ago(time_ago_in_minutes)
-  if time_ago_in_minutes >=60
-    "#{time_ago_in_minutes / 60} hours ago"
-  else
-    "#{time_ago_in_minutes} minutes ago"
-  end
-end
-
 get '/' do
   @finstagram_posts = FinstagramPost.order(created_at: :desc)
   erb(:index)
 end
   
+get '/signup' do
+
+  # grab uesr input values from params
+  email      = params[:email]
+  avatar_url = params[:avatar_url]
+  username   = params[:username]
+  password   = params[:password]
+
+  # if all user params are present
+  if email.present? && avatar_url.present? && username.present? && password.present?
+
+    # instantiate and save a User
+    user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
+    user.save
+
+    # return readable representation of User object
+    escape_html user.inspect
+
+  else
+
+    # display simple error message
+    "Validation failed."
+  end
+
+
+end
+
+post '/signup' do
+  email      = params[:email]
+  avatar_url = params[:avatar_url]
+  username   = params[:username]
+  password   = params[:password]
+
+  @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
+
+  if @user.save
+    "User #{username} saved!"
+  else
+    erb(:signup)
+  end
+end
